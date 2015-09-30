e347b5cc49e2c5226d6863fe74cf4643192ee5ba
Daniel Erickson <daniel@literacyplanet.com>
2015-09-30 15:13:49 +1000

    Remove unnecessary whitespace

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 import K from 'kefir'
 import sockjs from 'sockjs-client'
 
+// Init connection to the node server
 const sock = new sockjs('http://localhost:9090/echo')
 
+// Dispatch a message
 const sendMessage = message => {
   sock.send(JSON.stringify(message)) 
 }
@@ -11,16 +13,10 @@ const updateChat = message => {
   document.querySelector('#chat-content').value += ('User ' + message.username + ': ' + message.message + '\n')
 }
 
-// Trace out open and close of socket.
-sock.onopen = () => console.log('open')
-sock.onclose = () => console.log('close')
-
-
+// When we receive a message, will be refactored later to utilise Kefir and validate it
 sock.onmessage = message => updateChat(JSON.parse(message.data))
 
-
-
-// Event listeners from 
+// Event listeners 
 const sendClick$ = K.fromEvents(document.querySelector('#send'), 'click')
 
 const usernameValue$ = K.fromEvents(document.querySelector('#username'), 'keyup')
@@ -29,12 +25,10 @@ const usernameValue$ = K.fromEvents(document.querySelector('#username'), 'keyup'
 const messageValue$ = K.fromEvents(document.querySelector('#message'), 'keyup')
   .map(event => event.target.value);
 
-
 // On click merge the username and message values
 const result$ = K.combine([sendClick$],[usernameValue$,messageValue$],(click,username,message) => {
   return {username:username,message:message}
 })
-
 
 // For fun only send a message if the username and message field have values
 result$.filter(message =>  message.username!=='' && message.message !=='')
