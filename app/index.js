302e62f588e6662e73a2ff1f653829e2883ef914
Daniel Erickson <daniel@literacyplanet.com>
2015-09-30 16:27:43 +1000

    Update to use Kefir when receiving a message

--- app/index.js
+++ app/index.js
@@ -5,16 +5,10 @@ import sockjs from 'sockjs-client'
 const sock = new sockjs('http://localhost:9090/echo')
 
 // Dispatch a message
-const sendMessage = message => {
-  sock.send(JSON.stringify(message)) 
-}
+const sendMessage = message => sock.send(JSON.stringify(message)) 
 
-const updateChat = message => {
+const updateChat = message => 
   document.querySelector('#chat-content').value += ('User ' + message.username + ': ' + message.message + '\n')
-}
-
-// When we receive a message, will be refactored later to utilise Kefir and validate it
-sock.onmessage = message => updateChat(JSON.parse(message.data))
 
 // Event listeners 
 const sendClick$ = K.fromEvents(document.querySelector('#send'), 'click')
@@ -26,10 +20,14 @@ const messageValue$ = K.fromEvents(document.querySelector('#message'), 'keyup')
   .map(event => event.target.value);
 
 // On click merge the username and message values
-const result$ = K.combine([sendClick$],[usernameValue$,messageValue$],(click,username,message) => {
-  return {username:username,message:message}
-})
-
-// For fun only send a message if the username and message field have values
-result$.filter(message =>  message.username!=='' && message.message !=='')
+K.combine([sendClick$],[usernameValue$,messageValue$],(click,username,message) => ({
+    username:username,
+    message:message
+  }))
+  .filter(message =>  message.username!=='' && message.message !=='')
   .onValue(message => sendMessage(message))
+
+// On Message update the chat
+K.stream(stream => sock.onmessage = message => stream.emit(message))
+  .map(message => message.data)
+  .onValue(message => updateChat(JSON.parse(message)))
